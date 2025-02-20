cc5901a051d02341c07b7b8b9c1406f16fb37b42
arocchet <adrieen.rocchetti@gmail.com>
2025-02-20 14:41:36 +0100

    init project

---
+++ server/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	server := http.Server{
+		Addr:              ":8080",
+		Handler:           mux,
+		MaxHeaderBytes:    1 << 26,
+		ReadTimeout:       30 * time.Second,
+		ReadHeaderTimeout: 30 * time.Second,
+		WriteTimeout:      45 * time.Second,
+		IdleTimeout:       3 * time.Minute,
+	}
+
+	mux.HandleFunc("/", nil)
+
+	log.Println("Server started on http://localhost:8080")
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatalf("failed to start server: %s", err)
+	}
+
+}
