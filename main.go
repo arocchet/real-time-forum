877ec25e37b267cb54ba89b7667c810a425b9640
cnuttens <clement.nutt@gmail.com>
2025-02-21 11:03:05 +0100

    init serv

--- main.go
+++ main.go
@@ -2,21 +2,26 @@ package main
 
 import (
 	"log"
+	"main/server/handlers"
 	"net/http"
+	"os"
 	"time"
 )
 
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "./static/pages/index.html")
-}
 
 func main() {
+
+	port := "8080"
+
+	if len(os.Args ) > 1 {
+		port = os.Args[1]
+	}
 
 	mux := http.NewServeMux()
 	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
 	server := http.Server{
-		Addr:              ":8080",
+		Addr:              ":"+ port,
 		Handler:           mux,
 		MaxHeaderBytes:    1 << 26,
 		ReadTimeout:       30 * time.Second,
@@ -25,9 +30,9 @@ func main() {
 		IdleTimeout:       3 * time.Minute,
 	}
 
-	mux.HandleFunc("/", HomeHandler)
+	mux.HandleFunc("/", handlers.HomeHandler)
 
-	log.Println("Server started on http://localhost:8080")
+	log.Println("Server started on http://localhost:"+port)
 
 	err := server.ListenAndServe()
 	if err != nil {
